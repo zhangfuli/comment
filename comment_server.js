4afb6b47addc4b14c911ef35a4acabbc3fd4c20c
DESKTOP-8RMKUQ8\11710 <1171067930@qq.com>
2017-02-15 20:54:13 +0800

    路由&controller

---
+++ comment_server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var db = mongoose.connect('mongodb://localhost/comments');
+require('./models/comments_models.js');
+require('./models/photo_models.js');
+require('./models/page_models.js');
+var app = express();
+app.engine('.html',require('ejs').__express);
+app.set('views',__dirname + '/views');
+app.set('view engine','html');
+app.use(bodyParser());
+require('./commnet_routes')(app);
+app.listen(80);
